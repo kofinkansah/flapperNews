756b9f073b06c15bd91f87680257f1a00badbc8c
Kofi <kofinkansah@gmail.com>
2016-06-18 08:57:13 -0400

    Added to comments section

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 angular.module('flapperNews', ['ui.router'])
+
 .config([
 '$stateProvider',
 '$urlRouterProvider',
@@ -9,6 +10,12 @@ function($stateProvider, $urlRouterProvider) {
 			url: '/home',
 			templateUrl: '/home.html',
 			controller: 'MainCtrl'
+		});
+
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
 		});
 
 	$urlRouterProvider.otherwise('home'); //if the app recieves an undefined URL. 
@@ -31,7 +38,11 @@ function($stateProvider, $urlRouterProvider) {
 			$scope.posts.push({
 				title: $scope.title, 
 				link: $scope.link, 
-				upvotes: 0 
+				upvotes: 0,
+				comments: [
+					{author: 'Joe', body: 'Cool post!', upvotes: 0},
+					{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+				] 
 			});
 			$scope.title = '';
 			$scope.link ='';
@@ -41,13 +52,19 @@ function($stateProvider, $urlRouterProvider) {
 		};
 	}]);
 
-
-/*
-[
-			{title: 'post 1', upvotes: 5},
-			{title: 'post 2', upvotes: 2},
-			{title: 'post 3', upvotes: 15},
-			{title: 'post 4', upvotes: 9},
-			{title: 'post 5', upvotes: 4}
-		]
-*/
+.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+	$scope.post = posts.posts[$stateParams.id];
+	$scope.addComment = function(){
+		if($scope.body === '') { return; }
+		$scope.post.comments.push({
+			body: $scope.body,
+			author: 'user',
+			upvotes: 0
+		});
+		$scope.body = '';
+	};
+}]);
