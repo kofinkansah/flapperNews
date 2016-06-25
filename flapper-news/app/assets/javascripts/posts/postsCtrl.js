d71637c86c10bcb926ceb21abc0d5a9a216738e0
Kofi <kofinkansah@gmail.com>
2016-06-25 08:58:48 -0400

    Tied Rails together with Angular

--- flapper-news/app/assets/javascripts/posts/postsCtrl.js
+++ flapper-news/app/assets/javascripts/posts/postsCtrl.js
@@ -3,15 +3,19 @@ angular.module('flapperNews')
 '$scope',
 '$stateParams',
 'posts',
-function($scope, $stateParams, posts){
-	$scope.post = posts.posts[$stateParams.id];
+function($scope, posts, post){
+	$scope.post = posts;
 	$scope.addComment = function(){
-		if($scope.body === '') { return; }
-		$scope.post.comments.push({
-			body: $scope.body,
-			author: 'user',
-			upvotes: 0
-		});
-		$scope.body = '';
+	  if($scope.body === '') { return; }
+	  posts.addComment(post.id, {
+	    body: $scope.body,
+	    author: 'user',
+	  }).success(function(comment) {
+	    $scope.post.comments.push(comment);
+	  });
+	  $scope.body = '';
 	};
+	$scope.incrementUpvotes = function(comment){
+  posts.upvoteComment(post, comment);
+};
 }]);
